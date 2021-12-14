fc1c3136300a48c79a1bbf542e6a687305a47c5e
frodiBjorn <fedotovsky96@gmail.com>
2021-12-14 12:45:42 +0300

    add: add some architecture

---
+++ src/components/app/app.ts
@@ -0,0 +1,16 @@
+import Header from "../header/header";
+
+class App {
+    private container: HTMLElement;
+    private header: Header;
+
+    constructor(){
+        this.container = document.body;
+        this.header = new Header('header');
+    }
+    run() {
+        const headerHTML = this.header.render();
+        this.container.append(headerHTML)
+    }
+}
+export default App;
